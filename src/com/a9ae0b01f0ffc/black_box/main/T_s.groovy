2197045f567708cb3a1f59f1b37bda1fc27ff419
anton.pryamostanov <anton.pryamostanov@gmail.com>
2017-02-11 00:52:16 +0400

    Z

--- src/com/a9ae0b01f0ffc/black_box/main/T_s.groovy
+++ src/com/a9ae0b01f0ffc/black_box/main/T_s.groovy
@@ -3,10 +3,10 @@ package com.a9ae0b01f0ffc.black_box.main
 import com.a9ae0b01f0ffc.black_box.interfaces.I_logger
 import com.a9ae0b01f0ffc.black_box.interfaces.I_trace
 import com.a9ae0b01f0ffc.commons.ioc.T_class_loader
-import com.a9ae0b01f0ffc.commons.main.T_common_shortcuts
 import com.a9ae0b01f0ffc.commons.static_string.T_static_string
+import com.a9ae0b01f0ffc.commons.static_string.T_static_string_builder
 
-class T_s extends T_common_shortcuts {
+class T_s {
 
     static I_logger l() {
         return x().get_logger()
@@ -32,6 +32,14 @@ class T_s extends T_common_shortcuts {
 
     static T_class_loader ioc() {
         return x().get_ioc()
+    }
+
+    static final T_logging_commons commons() {
+        return T_logging_context.get_context().get_commons()
+    }
+
+    static final T_static_string_builder s() {
+        return T_logging_const.GC_STATIC_STRING_BUILDER
     }
 
 }
